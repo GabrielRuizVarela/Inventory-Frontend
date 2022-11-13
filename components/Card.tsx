ef34fca9f9053a57303520bd803f8ef9cff897b4
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-11-13 20:22:12 -0300

    Imporoved style

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,28 +6,31 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Item } from "../pages/index";
+import { CardActionArea } from "@mui/material";
 
 export default function MediaCard({ item }: { item: Item }) {
 	return (
-		<Card sx={{ maxWidth: 345 }}>
-			<CardMedia
-				component="img"
-				height="140"
-				image="/stock-photo.jpg"
-				alt="green iguana"
-			/>
-			<CardContent>
-				<Typography gutterBottom={true} variant="h5" component="div">
-					{item.name}
-				</Typography>
-				<Typography variant="body2" color="text.secondary">
-					{item.description}
-				</Typography>
-			</CardContent>
-			<CardActions>
-				<Button size="small">Price: {item.price}</Button>
-				<Button size="small">Stock: {item.stock}</Button>
-			</CardActions>
-		</Card>
+		<CardActionArea sx={{ maxWidth: 345 }}>
+			<Card>
+				<CardMedia
+					component="img"
+					height="140"
+					image="/stock-photo.jpg"
+					alt="green iguana"
+				/>
+				<CardContent>
+					<Typography gutterBottom={true} variant="h5" component="div">
+						{item.name}
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						{item.description}
+					</Typography>
+				</CardContent>
+				<CardActions>
+					<Button size="small">Price: {item.price}</Button>
+					<Button size="small">Stock: {item.stock}</Button>
+				</CardActions>
+			</Card>
+		</CardActionArea>
 	);
 }
