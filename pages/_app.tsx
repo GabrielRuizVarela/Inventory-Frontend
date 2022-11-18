9438060245ca90b0c7656b0bedc7a88022b9ff5c
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-11-18 11:38:03 -0300

    SearchBar rewrite

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,89 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { createContext, useMemo, useState } from "react";
 
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#fff1f0",
+      light: "#ff7961",
+      dark: "#ba000d",
+    },
+
+    secondary: {
+      main: "#3ea6ff",
+      light: "#72d1ff",
+      dark: "#007ac1",
+    },
+  },
+
+});
+
+export const ColorModeContext = createContext({ toggleColorMode: () => { } });
+export const AppContext = createContext({
+  view: "grid",
+  setView: (view: "grid" | "list") => { },
+  isDetailPage: false,
+  setIsDetailPage: (isDetailPage: boolean) => { },
+  isRemoveMode: false,
+  setIsRemoveMode: (removeMode: boolean) => { },
+});
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	);
+  const [mode, setMode] = useState<'light' | 'dark'>('light');
+  const [view, setView] = useState('grid');
+  const [isDetailPage, setIsDetailPage] = useState(false);
+  const [isRemoveMode, setIsRemoveMode] = useState(false);
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          primary: {
+            main: "#fff1f0",
+            light: "#ff7961",
+            dark: "#ba000d",
+          },
+
+          secondary: {
+            main: "#3ea6ff",
+            light: "#72d1ff",
+            dark: "#007ac1",
+          },
+        },
+      }),
+    [mode],
+  );
+  const appContext = useMemo(
+    () => ({
+      view,
+      setView,
+      isDetailPage,
+      setIsDetailPage,
+      isRemoveMode,
+      setIsRemoveMode,
+    }),
+    [view, isDetailPage, isRemoveMode],
+  );
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <AppContext.Provider value={appContext}>
+        <ThemeProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </AppContext.Provider>
+    </ColorModeContext.Provider>
+  );
 }
